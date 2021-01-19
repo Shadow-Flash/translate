6e3e3c89f609ef9c691eebbe8a467fee6e46075e
Shadow-Flash <varung848@gmail.com>
2021-01-19 18:39:00 +0530

    Pushing

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Button extends Component {
+    render() {
+        return (
+            <button className="ui primary">
+                Submit
+            </button>
+        )
+    }
+}
